fe55b6dc84fbe94a36097640b8f2a73284df0ab1
Karabo Maila <mailakarabo3@gmail.com>
2023-07-15 18:10:51 +0200

    updates

--- components/PageHead.js
+++ components/PageHead.js
@@ -8,10 +8,10 @@ export default function PageHead({ title = MetaData.siteName, description = Meta
         <Head>
             <title>{title}</title>
             <meta name="google-site-verification" content="IN46jp8RZq0u1wIxJdb8kN6gEDs7GZzkB0TMOyZ3aY4" />
-            {/* <meta charSet='utf-8' /> */}
+            <meta charSet='utf-8' />
             <meta name="description" content={ description } />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
-            {/* <link rel="canonical" href={url} key="canonical" /> */}
+            <link rel="canonical" href={url} key="canonical" />
             <link rel="icon" href={url} />
             <link rel="icon" href="/favicon.ico" />
             <meta property='og:locale' content="en_US"/>
@@ -21,8 +21,8 @@ export default function PageHead({ title = MetaData.siteName, description = Meta
             <meta property='og:image' content={ logo } />
             <meta property='og:url' content={url} />
             {/* <meta name="keywords" content="my, page, keywords" /> */}
-            {/* <meta name="robots" content="index, follow" /> */}
-            {/* <meta name="author" content="Karabo Maila" /> */}
+            <meta name="robots" content="index, follow" />
+            <meta name="author" content="Karabo Maila" />
         </Head>
     )
 }
