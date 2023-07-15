ce69ff8c0f7d6786119776703a2b787bc9c1c166
Karabo Maila <mailakarabo3@gmail.com>
2023-07-15 17:54:08 +0200

    updates

--- components/PageHead.js
+++ components/PageHead.js
@@ -7,11 +7,11 @@ export default function PageHead({ title = MetaData.siteName, description = Meta
     return (
         <Head>
             <title>{title}</title>
-            <meta name="google-site-verification" content="IN46jp8RZq0u1wIxJdb8kN6gEDs7GZzkB0TMOyZ3aY4" />
-            <meta charSet='utf-8' />
+            {/* <meta name="google-site-verification" content="IN46jp8RZq0u1wIxJdb8kN6gEDs7GZzkB0TMOyZ3aY4" /> */}
+            {/* <meta charSet='utf-8' /> */}
             <meta name="description" content={ description } />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link rel="canonical" href={url} key="canonical" />
+            {/* <link rel="canonical" href={url} key="canonical" /> */}
             <link rel="icon" href={url} />
             <link rel="icon" href="/favicon.ico" />
             <meta property='og:locale' content="en_US"/>
@@ -20,8 +20,9 @@ export default function PageHead({ title = MetaData.siteName, description = Meta
             <meta property='og:type' content={ ogType } />
             <meta property='og:image' content={ logo } />
             <meta property='og:url' content={url} />
- 
-            <meta name="author" content="Karabo Maila" />
+            {/* <meta name="keywords" content="my, page, keywords" /> */}
+            {/* <meta name="robots" content="index, follow" /> */}
+            {/* <meta name="author" content="Karabo Maila" /> */}
         </Head>
     )
 }
